78d22799a75cc36aecbb794bb3f2a743f673328f
gronk-droid <grantbutler7@gmail.com>
2022-03-17 10:38:43 -0400

    added lab-8.1

---
+++ lab-8.1/lab-8.1IV.asm
@@ -0,0 +1,41 @@
+; Demonstrate local variables   (LocalVars.asm)
+; This program demonstrates the use of local variables.
+INCLUDE Irvine32.inc
+.data
+.code
+main PROC
+    call    MySub
+    exit
+main ENDP
+
+X_local EQU DWORD PTR [ebp-4]
+Y_local EQU DWORD PTR [ebp-8]
+
+MySub PROC
+    ; push    ebp
+    ; mov    ebp,esp
+    ; sub    esp,8        ; create variables
+    enter 8, 0              ; two double words instead of regular prefix
+    mov    X_local,20    ; X
+    mov    Y_local,10    ; Y
+    ; mov    esp,ebp        ; remove locals from stack
+    ; pop    ebp
+    leave
+    ret
+MySub ENDP
+
+
+END main
+
+; ALTERNATE VERSION:
+
+MySub PROC
+    push    ebp
+    mov    ebp,esp
+    sub    esp,8                ; create variables
+    mov    DWORD PTR [ebp-4],20    ; X
+    mov    DWORD PTR [ebp-8],10    ; Y
+    mov    esp,ebp                ; remove locals from stack
+    pop    ebp
+    ret
+MySub ENDP
