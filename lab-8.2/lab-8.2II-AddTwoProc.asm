9e4b390364b1295a710e27f9b170921b6811c673
gronk-droid <grantbutler7@gmail.com>
2022-03-22 10:35:18 -0400

    part 2 init

---
+++ lab-8.2/lab-8.2II-AddTwoProc.asm
@@ -0,0 +1,15 @@
+INCLUDE AddTwo.inc
+
+AddTwo PROC
+; Adds two integers, returns sum in EAX.
+; The RET instruction cleans up the stack.
+
+    push ebp
+    mov  ebp,esp
+    mov  eax,[ebp + 12]       ; first parameter
+    add  eax,[ebp + 8]        ; second parameter
+    pop  ebp
+    ret  8                ; clean up the stack
+AddTwo ENDP
+
+END
