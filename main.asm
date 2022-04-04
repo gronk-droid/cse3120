0846fb3e79773b05ce248080f64398028bb2811f
Grant Butler <grantbutler7@gmail.com>
2022-04-04 19:13:39 -0400

    timer finished

--- main.asm
+++ main.asm
@@ -17,6 +17,13 @@ game_title BYTE "The Ultimate Typing Test",0
 
 user_input_string BYTE 40 DUP(?)
 
+start_tick DWORD ?
+elapsed_time DWORD ?
+seconds REAL4 ?
+timer_string BYTE ?
+
+ms_conversion REAL4 0.001
+
 .code
     main PROC
         ; Print Start Info
@@ -25,10 +32,10 @@ user_input_string BYTE 40 DUP(?)
 
         ; wait (seconds x 1000) and clear everything
         INVOKE Sleep, 5000
-        call Clrscr 
+        call Clrscr
 
         ; Set console title name
-        INVOKE SetConsoleTitle, ADDR game_title 
+        INVOKE SetConsoleTitle, ADDR game_title
 
         ; Get user input for number of chars
         mov edx, offset size_selection_string
@@ -47,10 +54,10 @@ user_input_string BYTE 40 DUP(?)
             inc edx
 
             cmp edx, string_size ; THIS VALUE DECIDES WHAT HOW MANY CHARS WE GET
-	        
+
             ; jump while we are below, this allows us to add one extra space at the end of the newline
             jb make_random_string
-            
+
             ; add the newline to the end
             mov random_string[edx], 0Ah
 
@@ -70,9 +77,9 @@ user_input_string BYTE 40 DUP(?)
 
         mov ebx, 0 ; our looping value
         check_if_correct_loop:
-            
+
             xor al, al ; set it to 0
-            
+
             ; move for comparison and do the comparison between words
             mov al, user_input_string[ebx]
             cmp al, random_string[ebx]
@@ -88,12 +95,12 @@ user_input_string BYTE 40 DUP(?)
             correct_letter:
                 mov edx, OFFSET get_input_string
                 call WriteString
-            
+
             ; Loop if needed
             inc ebx
             cmp ebx, string_size
             jb check_if_correct_loop
-        
+
 
         ; wait before exit
         call WaitMsg
@@ -106,10 +113,40 @@ user_input_string BYTE 40 DUP(?)
         ; start in lowercase and get a random in the range
         mov eax, 26
         call RandomRange
-            
+
         ; make it uppercase
         add eax, 65
 	    ret
     GenerateReandomChar ENDP
+
+    ; records the starting tick of the typing test and sets timer to 0
+    startTimer PROC
+        ; set starting tick
+        call GetTickCount
+        mov start_tick, eax
+
+        ; set timer to 0
+        mov timer, 0
+    startTimer ENDP
+
+    ; record end tick and set timer to time
+    endTimer PROC
+        ; get ending time of program
+        call GetTickCount
+        mov elapsed_time, eax
+
+        ; (end - start) = elapsed time in ms
+        sub elapsed_time, start_tick
+
+        ; ms →  s = (t ms)*0.001
+        finit                           ; initialize floating point processor
+        fld DWORD PTR [elapsed_time]    ; push elapsed_time onto stack
+        fld DWORD PTR [ms_conversion]   ; push conversion onto stack
+        fmul                            ; multiply
+        fstp DWORD PTR [seconds]        ; store as REAL4 in seconds
+
+        mov timer_running, FALSE
+
+    endTimer ENDP
 
 END main
